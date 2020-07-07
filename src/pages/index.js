61320052bfcf8df1e2c32cb44fab4d1cd3f2e1c5
kirill <axe777@inbox.ru>
2020-07-07 17:36:51 +0300

    offers types block

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import Layout from "../components/Layout/Layout"
 import HeaderWrapper from "../components/HeaderWrapper/HeaderWrapper"
 import Projects from "../components/Projects/Projects"
+import Offers from "../components/Offers/Offers"
 
 export default function Home() {
   return (
     <Layout>
       <HeaderWrapper />
       <Projects />
+      <Offers />
     </Layout>
   )
 }
